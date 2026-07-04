ee48a2cb6bf228912af7290ff4ded05a0d5da928
Shaneel Singh <shaneelsingh@capitecbank.co.za>
2026-07-04 19:16:45 +0200

    faet: add ITransactionSource interface - adaptor contract

---
+++ ITransactionSource.cs
@@ -0,0 +1,10 @@
+﻿using TransactionAggregationAPI.Application.Domain;
+
+namespace TransactionAggregationAPI.Application.Interfaces;
+
+public interface ITransactionSource
+{
+    string SourceName { get; }
+
+    Task<IReadOnlyList<UnifiedTransaction>> FetchTransactionsAsync(CancellationToken ct = default);
+}
